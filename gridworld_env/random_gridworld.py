ab63cdd0d00666c3577468f4e4508086510c2288
Ethan Brooks <ethanabrooks@gmail.com>
2019-02-08 16:55:25 -0500

    fixed up random_gridworld so that everything works

--- gridworld_env/random_gridworld.py
+++ gridworld_env/random_gridworld.py
@@ -10,27 +10,23 @@ from gridworld_env.gridworld import GridWorld
 class RandomGridWorld(GridWorld):
     def __init__(self, random: Dict[str, int] = None, *args, **kwargs):
         self.random = random
-        self.potential_new = None
+        self.possible_choices = None
         super().__init__(*args, **kwargs)
-        self.potential_new = np.ravel_multi_index(
-            np.where(
-                np.logical_not(
-                    np.logical_or(
-                        np.isin(self.desc, self.blocked),
-                        np.isin(self.desc, self.start)))),
+        self.possible_choices = np.ravel_multi_index(
+            np.where(np.logical_not(np.isin(self.desc, self.blocked))),
             dims=self.desc.shape)
-        assert self.potential_new.size
+        assert self.possible_choices.size
         self.observation_space = spaces.Tuple(
             [self.observation_space] * (1 + len(random)))
 
     def append_randoms(self, state):
-        return tuple(
-            [int(np.squeeze(s)) for s in [state] + self.random_states])
+        return (state, *map(int, self.random_states))
 
     def set_randoms(self):
         n_choices = sum(self.random.values())
+        possible_choices, = np.where(self.possible_choices != self.s)
         choices = np.random.choice(
-            self.potential_new, size=n_choices, replace=False)
+            possible_choices, size=n_choices, replace=False)
         *self.random_states, _ = np.split(
             choices, np.cumsum(list(self.random.values())))
 
@@ -38,9 +34,10 @@ class RandomGridWorld(GridWorld):
         self.set_desc(self.desc)
 
     def reset(self):
+        o = super().reset()
         self.set_randoms()
         self.last_transition = None
-        return self.append_randoms(super().reset())
+        return self.append_randoms(o)
 
     def set_desc(self, desc):
         self.P = self.get_transitions(desc)
